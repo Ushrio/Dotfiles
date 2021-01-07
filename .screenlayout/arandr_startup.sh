8847803e98f736a140dc951f00075242eda99588
Greg Heiman <gregheiman02@gmail.com>
2021-01-06 18:54:17 -0600

    Added dunst and Zathura configurations

--- .screenlayout/arandr_startup.sh
+++ .screenlayout/arandr_startup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --off --output DisplayPort-1 --mode 1920x1080 --rate 144.00 --pos 1920x0 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-D-0 --off
+xrandr --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --rate 165.00 --output HDMI-A-0 --off --output DVI-D-0 --mode 1920x1080 --pos 0x0 --rotate normal --rate 60.00
