7819d6088469a057c92da22a346a39e3ed033ac4
Greg Heiman <gregheiman02@gmail.com>
2021-11-28 23:48:29 -0600

    Updated auto_setup.sh script to include new systemd services.

--- scripts/auto_setup.sh
+++ scripts/auto_setup.sh
@@ -39,6 +39,8 @@ clang\
 clang-tools-extra\
 ripgrep\
 tmux\
+maven\
+gdb\
 
 # Install all of the required texlive packages
 sudo dnf install -y \
@@ -51,9 +53,7 @@ texlive-capt-of \
 # Set up Snap and the snap packages
 sudo dnf install -y snapd
 sudo ln -s /var/lib/snapd/snap /snap
-sudo snap install -y emacs --edge --classic
 sudo snap install -y spotify
-sudo snap install -y universal-ctags
 
 # Symlink all the important files
 mkdir ~/.config/git
@@ -68,8 +68,13 @@ ln -s ~/Dotfiles/bash/.bashrc ~/.bashrc
 ln -s ~/Dotfiles/.tmux.conf ~/.tmux.conf
 
 # Move the systemd services
-sudo cp ~/Dotfiles/systemd_services/rclone.service /etc/systemd/system/rclone.service
-sudo cp ~/Dotfiles/systemd_services/onedrivewsu.service /etc/systemd/system/onedrivewsu.service
+mkdir -p ~/.config/systemd/user
+ln -s ~/Dotfiles/systemd_services/rclone.service ~/.config/systemd/user/rclone.service
+ln -s ~/Dotfiles/systemd_services/onedrivewsu.service ~/.config/systemd/onedrivewsu.service
+ln -s ~/Dotfiles/systemd_services/emacs.service ~/.config/systemd/emacs.service
+systemctl --user enable rclone.service
+systemctl --user enable onedrivewsu.service
+systemctl --user enable emacs.service 
 
 # Create Rclone Directories
 mkdir ~/OneDrive\ -\ Personal
@@ -89,35 +94,6 @@ ln -s ~/Emacs-Config/early-init.el ~/.emacs.d/early-init.el
 ln -s ~/Emacs-Config/Custom ~/.emacs.d/Custom
 ln -s ~/Emacs-Config/mu4e ~/.emacs.d/mu4e
 
-# Download mu4e dependencies
-sudo dnf install -y gmime30-devel\
-xapian-core-devel\
-html2text\
-xdg-utils\
-guile22-devel\
-webkitgtk3-devel\
-meson\
-cmake\
-autoconf\
-automake\
-texinfo\
-# Download the mu git repo and build
-git clone git://github.com/djcb/mu.git
-cd mu
-meson build && ninja -C build
-./autogen.sh && make
-sudo make install
-rm -d -r ~/mu
-
-# Create maildirs
-mkdir -p ~/Mail/acc1-gmail
-mkdir -p ~/Mail/acc2-gmail
-
-# Run the commands to sync email and index with mu
-mbsync -c ~/.emacs.d/mu4e/.mbsyncrc -a
-mu init --maildir=~/Mail --my-address=gregheiman02@gmail.com --my-address=treebark1378@gmail.com
-mu index
-
 # Symlink all of the files from Vim config
 ln -s ~/Vim-Config/.vimrc ~/.vim/.vimrc
 ln -s ~/Vim-Config/after ~/.vim/after
@@ -128,6 +104,32 @@ ln -s ~/Vim-Config/nvim ~/.config/nvim
 git clone https://github.com/vinceliuice/Tela-icon-theme.git
 sh ~/Tela-icon-theme/install.sh -d ~/.icons
 rm -d -r ~/Tela-icon-theme
+
+mkdir ~/Programs
+# Download and build universal ctags
+git clone https://github.com/universal-ctags/ctags.git ~/Programs/
+cd ~/Programs/ctags
+./autogen.sh
+./configure
+make
+sudo make install # may require extra privileges depending on where to install
+
+# Download and build emacs
+sudo dnf install -y jansson-devel mpfr-devel libmpc-devel gmp-devel libgccjit-devel \
+    libungif-compat-devel libXpm-devel libjpeg-devel libpng-devel gtk3-devel libtiff-devel \
+    dbus-devel ncurses-devel
+git clone https://github.com/flatwhatson/emacs ~/Programs/
+cd ~/Programs/emacs
+./autogen.sh
+./configure -C --with-dbus --with-gif --with-jpeg --with-png --with-rsvg \
+    --with-tiff --with-gnutls --with-xft --with-xpm --with-gpm=no --with-modules --with-native-compilation \
+    --with-pgtk CFLAGS='-O2 -march=native' --program-transform-name=s/^ctags$/ctags_emacs/
+make -j6
+sudo make install
+systemctl -user start emacs
+
+# Install python packages
+pip install --user pynvim
 
 # Setup new root password
 sudo passwd
